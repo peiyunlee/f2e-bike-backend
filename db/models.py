67d2e1d7708599541091edbab3a7628132e20ed5
may <may1092200258@gmail.com>
2021-12-24 02:50:39 +0800

    fix

--- db/models.py
+++ db/models.py
@@ -1,7 +1,8 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relation, relationship
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.sql.schema import ForeignKey
+
 
 # class DbStationItem(Base):
 #     __tablename__ = 'station_item'
@@ -17,18 +18,18 @@ class DbRouteItem(Base):
     city = Column(String, nullable=False)
     routename = Column(String, nullable=False)
     store_id = Column(Integer, ForeignKey('store.id'))
-    
 
 class DbStore(Base):
     __tablename__ = 'store'
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('user.id'))
+    username = Column(Integer, ForeignKey('user.username'))
     route_items = relationship('DbRouteItem')
     # station_items = relationship('DbStationItem')
-
 
 class DbUser(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    username = Column(String(30), unique=True, nullable=False)
+    email = Column(String(30), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
