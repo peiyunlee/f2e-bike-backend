a76fd80dda7bace8978abd11a990a0e754b0c2d7
Peiyun Lee <may1092200258@gmail.com>
2022-02-03 23:40:12 +0800

    fix

--- db/database.py
+++ db/database.py
@@ -6,7 +6,8 @@ import os
 DB_USERNAME = os.environ.get("DB_USERNAME")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_NAME}"
+# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://may3256@the-f2e-bike-may:Ntuedtd_bike@the-f2e-bike-may.mysql.database.azure.com/bike"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
